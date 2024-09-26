6068fa49ca6996fcbbf37ab5f6ffd3d6eb98d352
Mesum Raza Hemani <mes.javacca@gmail.com>
2024-09-26 22:15:54 +0500

    Update app3_download.py
    For Meetup 21

--- app3_download.py
+++ app3_download.py
@@ -1,180 +1,3 @@
-# # import streamlit as st
-# # from PIL import Image, ImageDraw, ImageFont
-# # import os
-
-# # def add_image_to_certificate(certificate_path, uploaded_image, text, output_path):
-# #     # Open the certificate image
-# #     certificate_img = Image.open(certificate_path)
-    
-# #     # Open the uploaded image
-# #     uploaded_img = Image.open(uploaded_image)
-    
-# #     # Resize the uploaded image
-# #     uploaded_img = uploaded_img.resize((400, 400))  # Change dimensions as needed
-    
-# #     # Calculate the position to place the uploaded image
-# #     position = (1130, 38)  # Change position as needed
-    
-# #     # Paste the uploaded image onto the certificate image
-# #     certificate_img.paste(uploaded_img, position)
-    
-# #     # Create a drawing context
-# #     draw = ImageDraw.Draw(certificate_img)
-    
-# #     # Custom font style and font size for text
-# #     font = ImageFont.truetype('arial.ttf', 40)
-    
-# #     # Calculate the position to place the text
-# #     if len(text) <= 5:
-# #         text_position = (1250, 450)
-# #     elif len(text) <= 10:
-# #         text_position = (1200, 450)
-# #     elif len(text) >= 15:
-# #         text_position = (1120, 450)
-# #     else:
-# #         # Default position if text length is not 5 or 15
-# #         text_position = (1200, 450)
-    
-# #     # Add text to the certificate image
-# #     draw.text(text_position, text, fill="black", font=font)
-    
-# #     # Save the edited image
-# #     certificate_img.save(output_path)
-
-# # def main():
-# #     st.title("Certificate Creator")
-    
-# #     # User input for uploading image
-# #     uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
-    
-# #     # User input for text
-# #     text = st.text_input("Enter text:")
-    
-# #     if st.button("Generate Certificate"):
-# #         if uploaded_image:
-# #             # Save the uploaded image temporarily
-# #             temp_image_path = "temp_image.png"
-# #             with open(temp_image_path, "wb") as f:
-# #                 f.write(uploaded_image.getvalue())
-            
-# #             # Call function to add image to the certificate
-# #             add_image_to_certificate('Certificate.jpg', temp_image_path, text, 'generated_certificate.png')
-            
-# #             # Display the edited certificate
-# #             st.image('generated_certificate.png', caption="Generated Certificate", use_column_width=True)
-            
-# #             # Add download button
-# #             st.download_button(
-# #                 label="Download Certificate",
-# #                 data=open('generated_certificate.png', 'rb').read(),
-# #                 file_name='generated_certificate.png',
-# #                 mime='image/png'
-# #             )
-            
-# #             # Remove the temporary image file
-# #             os.remove(temp_image_path)
-# #         else:
-# #             st.warning("Please upload an image.")
-
-# # if __name__ == "__main__":
-# #     main()
-
-
-# import streamlit as st
-# from PIL import Image, ImageDraw, ImageFont
-# import os
-
-# def add_image_to_certificate(certificate_path, uploaded_image, text, output_path):
-#     # Open the certificate image
-#     certificate_img = Image.open(certificate_path)
-    
-#     # Open the uploaded image
-#     uploaded_img = Image.open(uploaded_image)
-    
-#     # Resize the uploaded image
-#     uploaded_img = uploaded_img.resize((360, 330))  # Change dimensions as needed
-    
-#     # Create a round mask
-#     mask = Image.new("L", uploaded_img.size, 0)
-#     draw = ImageDraw.Draw(mask)
-#     draw.ellipse((0, 0, uploaded_img.size[0], uploaded_img.size[1]), fill=255)
-    
-#     # Apply the round mask to the uploaded image
-#     uploaded_img.putalpha(mask)
-    
-#     # Calculate the position to place the uploaded image
-#     position = (1150, 105)  # Change position as needed
-    
-#     # Paste the uploaded image onto the certificate image
-#     certificate_img.paste(uploaded_img, position, uploaded_img)
-    
-#     # Create a drawing context
-#     draw = ImageDraw.Draw(certificate_img)
-    
-#     # Custom font style and font size for text
-#     font = ImageFont.truetype('arial.ttf', 38)
-    
-#     # text_position = (1200, 465)
-
-#     # Calculate the position to place the text
-#     if len(text) <= 5:
-#         text_position = (1280, 460)
-#     elif len(text) <= 9:
-#         text_position = (1230, 460)
-#     elif len(text) <= 12:
-#         text_position = (1210, 460)
-#     elif len(text) <= 12:
-#         text_position = (1160, 460)
-#     elif len(text) <= 10:
-#         text_position = (1200, 460)
-
-#     else:
-#         # Default position if text length is not 5 or 15
-#         text_position = (1110, 460)
-    
-#     # Add text to the certificate image
-#     draw.text(text_position, text, fill="black", font=font)
-    
-#     # Save the edited image
-#     certificate_img.save(output_path)
-
-# def main():
-#     st.title("Banner Creator")
-    
-#     # User input for uploading image
-#     uploaded_image = st.file_uploader("Upload your Picture", type=["jpg", "jpeg", "png"])
-    
-#     # User input for text
-#     text = st.text_input("Enter your name:")
-    
-#     if st.button("Generate Banner"):
-#         if uploaded_image:
-#             # Save the uploaded image temporarily
-#             temp_image_path = "temp_image.png"
-#             with open(temp_image_path, "wb") as f:
-#                 f.write(uploaded_image.getvalue())
-            
-#             # Call function to add image to the certificate
-#             add_image_to_certificate('KAI-MEETUP-ATTEND-SHARE.jpg', temp_image_path, text, 'generated_banner.png')
-            
-#             # Display the edited certificate
-#             st.image('generated_banner.png', caption="Banner Generated", use_column_width=True)
-            
-#             st.download_button(
-#                 label="Download Banner",
-#                 data=open('generated_banner.png', 'rb').read(),
-#                 file_name='generated_banner.png',
-#                 mime='image/png'
-#             )
-
-#             # Remove the temporary image file
-#             os.remove(temp_image_path)
-#         else:
-#             st.warning("Please upload your Picture.")
-
-# if __name__ == "__main__":
-#     main()
-
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
 import os
@@ -274,15 +97,16 @@ def main():
             f"""
             Dear {text},
 
-            We are thrilled to confirm your participation in the highly anticipated event: Karachi AI - Joining Google I/O Extended Karachi 2024 in partnership with GDG Kolachi! 🎉
+            We are thrilled to confirm your participation in the highly anticipated event : Karachi AI Meetup # 21 : Applied AI in Media / Entertainment Industry and Game Development with AI
             
             Please note 100 Certificates will be available on first come first serve basis for Registered Participants, Make sure to get your name noted!
             
-            Event Details: Karachi AI Meetup # 20 : Grand Meetup -  THE NEW MULTIMODAL AND AGENTIC AI
-
-            Date: Saturday, 27th July 2024, 10 AM - 2 PM Sharp @ IBA Main Campus, University Road, Karachi.
+            📅 Event Details:
+            - 📍 Location: Work More, 9th Floor, Fortune Towers, Shahra e Faisal, Karachi
+            - 🗓️ Date: Sunday, 29 Sept 2024
+            - 🕐 Time: 02:00 PM - 6:00 PM (Check-in for Certificates at 1.15 PM)
             
-            Stay Updated : https://www.facebook.com/groups/karachidotai
+            You're request to generated below banner for you and share on LinkedIn.
             
             Best Regards,
             **Team Karachi AI**
@@ -294,27 +118,27 @@ def main():
             
             st.markdown(
             """
-            🚀 Hey everyone! I'm thrilled to share an extraordinary event: Karachi AI - Joining Google I/O Extended Karachi 2024 in partnership with GDG Kolachi! 🌐✨
-            
-            Event Name : Karachi AI Meetup # 20 : Grand Meetup -  THE NEW MULTIMODAL AND AGENTIC AI
+            🚀 Hey everyone! I'm thrilled to share an extraordinary event Karachi AI Meetup # 21 : Applied AI in Media / Entertainment Industry and Game Development with AI
 
             📅 Event Details:
-            - 📍 Location: Institute of Business Administration, Main Campus, Karachi
-            - 🗓️ Date: Saturday, 27th July 2024
-            - 🕐 Time: 10:30 AM - 2:00 PM
+            - 📍 Location: Work More, 9th Floor, Fortune Towers, Shahra e Faisal, Karachi
+            - 🗓️ Date: Sunday, 29 Sept 2024
+            - 🕐 Time: 02:00 PM - 6:00 PM
+            
+            Join me to learn :
+            
+            1. AI in Media: Dive into how AI is transforming scriptwriting, scene generation, and post-production with tools like VFX, captions, and translations.
+            
+            2. AI in Gaming: Explore AI’s role in character creation, world-building, and game development, including personalized NPCs and AI-assisted testing.
+            
+            🎬 Let’s embark on this AI journey together! Don’t miss this chance to explore the next big thing in media and meet fellow creators and innovators!
+            
+            👉 Follow me to stay updated and get more details about this groundbreaking event. 
+            
+            Learn more about Karachi AI - https://www.karachidotai.com/community#meetups
+            
+            #GenerativeAI #FutureOfGaming #AIinGames #GameDevCommunity #NextGenGaming
 
-            🎯 Theme: THE NEW MULTIMODAL AND AGENTIC AI
-
-            Join us for an incredible journey into the future of artificial intelligence. This Grand Meetup will dive deep into transformative advancements, covering:
-
-            Artificial General Intelligence (AGI)
-            Multimodal AI featuring GPT-4o & Google's Project Astra
-            Agentic AI Planning Workflows with MS Co-Pilot & Crew AI
-            🔍 Discover cutting-edge innovations and gain insights from AI experts about AGI's potential and challenges, the integration of various data types for comprehensive AI, and the future of autonomous goal achievement with Agentic AI.
-
-            Don't miss out on this opportunity to explore the future of AI! 🚀
-
-            👉 Follow me to stay updated and get more details about this groundbreaking event. Let’s embark on this AI journey together! Learn more about Karachi AI - https://www.karachidotai.com/community
             """)
             
             st.divider()
